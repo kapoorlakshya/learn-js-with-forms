5c38604ea317c267a473deb9abb30defbd9d1348
kapoorlakshya <kapoorlakshya@gmail.com>
2017-12-27 02:10:28 -0800

    Add user registration confirmation alert.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,13 @@
-function myFunction() {
-    var app = document.getElementById("app");
-    app.style.backgroundColor = "red";
+"use strict";
+
+function confirmSubmit() {
+    var fName = document.getElementById("first_name").value;
+    var lName = document.getElementById("last_name").value;
+    var email = document.getElementById("email").value;
+
+    var msg = `New user registered successfully!\n\n` +
+        `First Name: ${fName}\n` +
+        `Last Name: ${lName}\n\n` +
+        `Check your email '${email}' for confirmation`;
+    alert(msg);
 }
