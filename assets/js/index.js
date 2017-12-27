4500848a226a48880feb0c0fa49149364315f213
kapoorlakshya <kapoorlakshya@gmail.com>
2017-12-27 02:47:11 -0800

    Add email validation.

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,3 +11,15 @@ function confirmSubmit() {
         `Check your email '${email}' for confirmation`;
     alert(msg);
 }
+
+function validateFieldValue(inputField, msgFieldId, val) {
+    if (!inputField.value.match(/.+@.+/)) {
+        var msgArea = document.getElementById(msgFieldId);
+        msgArea.innerText = `${inputField.name} validation failed! '@' is required.`;
+        msgArea.style.color = "red";
+        return true;
+    }
+    else {
+        return false;
+    }
+}
