6031a45ed06caa60ef78eb8381b53fef8db1d63c
kapoorlakshya <kapoorlakshya@gmail.com>
2017-12-28 04:00:29 -0800

    Improve email validation, add global notification, and general bug fixes in validations.

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,8 +13,9 @@ function validateFields() {
     let emailValidationMsgField = document.getElementById("email-validation-msg");
     let canSubmit = true; // All validations passed?
 
-    // Checking String length is faster than comparing with "" because the interpreter does not need to
-    // create a String object for ""
+    // Make sure Name and Email fields are not blank
+    // Note: Checking String length is faster than comparing with "" because the interpreter
+    // does not need to create a String object for ""
     // https://stackoverflow.com/questions/154059/how-do-you-check-for-an-empty-string-in-javascript
     if (!fName.value.length > 0) {
         fNameValidationMsgField.style.color = "red";
@@ -40,7 +41,14 @@ function validateFields() {
         emailValidationMsgField.innerText = "";
     }
 
+    // Verify email
+    if (confirmEmail() === false) {
+        canSubmit = false;
+    }
+
+    // All checks passed
     if (canSubmit) {
+        clearValidationMessages();
         let msg = "New user registered successfully!\n\n" +
                 `First Name: ${fName.value}\n` +
                 `Last Name: ${lName.value}\n\n` +
@@ -48,23 +56,15 @@ function validateFields() {
         alert(msg);
         return true;
     }
-
-    return false;
-}
-
-// Clear all fields validation messages
-function clearValidationMessages() {
-    let fNameValidationMsgField = document.getElementById("fname-validation-msg");
-    let lNameValidationMsgField = document.getElementById("lname-validation-msg");
-    let emailValidationMsgField = document.getElementById("email-validation-msg");
-
-    fNameValidationMsgField.innerText = "";
-    lNameValidationMsgField.innerText = "";
-    emailValidationMsgField.innerText = "";
+    else
+    {
+        displayFixErrorNotification();
+        return false;
+    }
 }
 
 // Validates email contains @
-function validateFieldValue(inputField, msgFieldId) {
+function validateEmail(inputField, msgFieldId) {
     let msgArea = document.getElementById(msgFieldId);
 
     // Look for @ and only validation when field is not empty
@@ -76,4 +76,45 @@ function validateFieldValue(inputField, msgFieldId) {
         msgArea.innerText = ""; // Nothing to display
         return false;
     }
+}
+
+// Confirm email by matching values from 2 email fields
+function confirmEmail() {
+    let emailField = document.getElementById("email-id");
+    let confirmEmailField = document.getElementById("confirm-email");
+    let confirmEmailValidationMsgField = document.getElementById("email-confirm-validation-msg");
+
+    if (confirmEmailField.value !== emailField.value) {
+        confirmEmailValidationMsgField.style.color = "red";
+        confirmEmailValidationMsgField.innerText = `${confirmEmailField.name} does not match.`;
+        return false;
+    }
+    else
+    {
+        confirmEmailValidationMsgField.innerText = "";
+        return true;
+    }
+}
+
+// Displays general error in page-messages Div
+function displayFixErrorNotification(){
+    let pageMessages = document.getElementById("page-messages");
+    pageMessages.style.color = "red";
+    pageMessages.innerText = "Fix the errors!";
+}
+
+// Clear all fields validation messages
+function clearValidationMessages() {
+    let pageMessages = document.getElementById("page-messages");
+    let fNameValidationMsgField = document.getElementById("fname-validation-msg");
+    let lNameValidationMsgField = document.getElementById("lname-validation-msg");
+    let emailValidationMsgField = document.getElementById("email-validation-msg");
+    let confirmEmailValidationMsgField = document.getElementById("email-confirm-validation-msg");
+
+    pageMessages.innerText = "";
+    fNameValidationMsgField.innerText = "";
+    lNameValidationMsgField.innerText = "";
+    emailValidationMsgField.innerText = "";
+    confirmEmailValidationMsgField.innerText = "";
+    return true;
 }
